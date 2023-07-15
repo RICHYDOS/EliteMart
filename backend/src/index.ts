e3b458305aa3c2c199a4f81c3c7d3f17429c27bf
Richard <richardosunmu@gmail.com>
2023-07-15 01:11:31 +0100

    feat: setup the server and connect to mongodb atlas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Have Fun Coding");
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+const app = express();
+
+const connectDb = async (): Promise<void> => {
+    try {
+        const connect = await mongoose.connect(process.env.CONNECTIONSTRING as string);
+        console.log("Connected to", connect.connection.name);
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+connectDb();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+
+app.listen(port, () => {
+    console.log(`Server running on Port ${port}`);
+})
