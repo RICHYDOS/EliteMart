716e3af5ceeb367e99f852ded5066f4a6ea9e4ec
IndyWebstars <honouradewunmi42@gmail.com>
2023-07-19 13:23:50 +0100

    modified all files that referenced the volunteer.jsx

--- frontend/GlobalKidCare/src/pages/index.js
+++ frontend/GlobalKidCare/src/pages/index.js
@@ -1,11 +1,9 @@
 import Error from "./error";
 import Access from "./access";
 import Landing from "./landing";
-import Volunteer from "./volunteer";
 
 export{
     Access,
     Error,
     Landing,
-    Volunteer,
 }
