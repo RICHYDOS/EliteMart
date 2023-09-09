0234b0c9982955beeedd75d16874629e71f5b9ec
Richard <richardosunmu@gmail.com>
2023-09-09 15:44:43 +0100

    feat: signup, login, view and update cart endpoints

--- backend/src/controllers/carts.ts
+++ backend/src/controllers/carts.ts
@@ -1,71 +1,25 @@
-import { Response } from 'express';
-import {requestWithUserData} from "../middleware/auth";
-import {createCart, displayCart} from '../dal/carts';
-import { cartStructure, Cart } from '../models/cart';
-import { Schema } from 'mongoose';
+import { Request, Response } from 'express';
+import EliteMart from '../models/db';
 
-export const create = async (req: requestWithUserData, res: Response): Promise<void> => {
-    if (req.currentUser){
-        const payload: cartStructure = {
-            user: req.currentUser.userDetails.id
-        }
-        const cartExists = await displayCart(req.currentUser.userDetails.id);
-        if (cartExists){
-            res.status(400);
-            throw new Error('Cart Exists');
-        }
-        const cart = await createCart(payload);
-        if (cart) {
-            res.status(201).send(cart);
-        } 
-        else {
-            res.status(400);
-            throw new Error('Invalid Data');
-        }
-    }
-    else{
-            res.status(400);
-            throw new Error('User Unauthorised');
-    }
-    
-    
-    
+export const display = async (req: Request, res: Response): Promise<void> => {
+  const user = res.locals.user;
+  res.status(200).send(user.cart);
 };
 
-export const display = async (req: requestWithUserData, res: Response): Promise<void> => {
-    if (req.currentUser){
-        const cart = await displayCart(req.currentUser.userDetails.id);
-        if (cart){
-            res.status(200).send(cart);
-        }
-        else {
-            res.status(404);
-            throw new Error('No Cart Found');
-        }
-    }
-    else{
-        res.status(400);
-        throw new Error('User Unauthorised');
-    }
-};
-
-export const update = async (req: requestWithUserData, res: Response): Promise<void> => {
-    if (req.currentUser){
-        const user = req.currentUser.userDetails.id;
-        const flavours: [{flavour:Schema.Types.ObjectId, name:string, count: number}] = req.body.flavours;
-        await Cart.findOneAndUpdate(
-            { user },
-            { $set: { flavours: [] } },
-          );
-        const cart = await Cart.findOneAndUpdate(
-            { user },
-            { $push: { flavours: { $each: flavours } } },
-            { new: true }
-          );
-        res.status(200).send(cart);
-    }
-    else{
-        res.status(400);
-        throw new Error('User Unauthorised');
-    }
+export const update = async (req: Request, res: Response): Promise<void> => {
+  const currentUser = res.locals.user;
+  const user = await EliteMart.findOne({
+    email: currentUser.email,
+  }).select('cart');
+  
+  if (!user) {
+    res.status(400);
+    throw new Error('Server Error');
+  }
+  user.cart = [];
+  for (const obj of req.body) {
+    user.cart.push(obj);
+  }
+  await user.save();
+  res.status(200).send({ status: 'Cart Updated' });
 };
--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -2,63 +2,57 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
 import { setting } from '../config/config';
-import {createUser, findUser} from '../dal/users';
-import {validate} from "../models/users";
-import { userInput } from '../models/users';
-import { cartStructure } from '../models/cart';
-import {createCart} from '../dal/carts';
+import { validateRegister, validateLogin } from '../models/db';
+import EliteMart from '../models/db';
 
 export const register = async (req: Request, res: Response): Promise<void> => {
-    const input: userInput = {
-        email: req.body.email,
-        password: req.body.password
-    }
-    const result = validate(input);
-    if (result.error) res.status(400).send(result.error.details[0].message);
-    
-    if (await findUser(input.email)) {
-        res.status(400);
-        throw new Error('You Already Exist. Login Instead');
-    } else {
-        const hashedPassword = await bcrypt.hash(input.password, 10);
-        const userObject = { email: input.email, password: hashedPassword };
-        const user = await createUser(userObject);
-        if (user) {
-          const payload: cartStructure = {
-            user: user._id
-          }
-          await createCart(payload);
-          res.status(201).send(user);
-        } 
-        else {
-            res.status(400);
-            throw new Error('Invalid Data');
-        }
-    }
+  const input = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  const result = validateRegister(input);
+  if (result.error) res.status(400).send(result.error.details[0].message);
+
+  if (await EliteMart.findOne({ email: input.email })) {
+    res.status(400);
+    throw new Error('You Already Exist. Login Instead');
+  }
+  const hashedPassword = await bcrypt.hash(input.password, 10);
+  try {
+    const user = await EliteMart.create({
+      name: input.name,
+      email: input.email,
+      password: hashedPassword,
+      cart: [],
+    });
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400);
+    throw new Error('Invalid Data');
+  }
 };
 
 export const login = async (req: Request, res: Response): Promise<void> => {
-  const input: userInput = {
-    email: req.body.email,
-    password: req.body.password
-}
-  const result = validate(input);
+  const { email, password } = req.body;
+  const result = validateLogin({ email, password });
   if (result.error) res.status(400).send(result.error.details[0].message);
 
-  const user = await findUser(input.email);
+  const user = await EliteMart.findOne({ email: email });
 
   if (user) {
     const hashedPassword: string = user.password;
 
     // Compare client password with db password
-    if (await bcrypt.compare(input.password, hashedPassword)) {
+    if (await bcrypt.compare(password, hashedPassword)) {
       const accessToken = jwt.sign(
         //Payload
         {
           userDetails: {
             id: user._id,
-            email: user.email
-          }
+            email: user.email,
+            cart: user.cart,
+          },
         },
         //Access Token Secret Key
         setting.secretKey,
